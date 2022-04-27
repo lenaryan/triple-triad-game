21c04c0ca7a7a9fd5602fb6fb89838689b2d5dca
Elena Nichutina <elena.nichutina@x5.ru>
2022-04-27 12:59:12 +0300

    rename prop

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,32 +1,32 @@
-import PropTypes from 'prop-types';
-import cn from 'classnames';
-import s from './Button.module.scss';
+import PropTypes from "prop-types";
+import cn from "classnames";
+import s from "./Button.module.scss";
 
-const Button = ({ blackbtn = false, className, children, onClick }) => {
-    const handleClick = () => {
-        onClick();
-    }
-    
-    return (
-        <button className={cn(
-            s.root, 
-            className, {
-                [s.black]: blackbtn
-            })} 
-            onClick={handleClick}>
-            {children}
-        </button>
-    )
-}
+const Button = ({ isBlack = false, className, children, onClick }) => {
+	const handleClick = () => {
+		onClick();
+	};
+
+	return (
+		<button
+			className={cn(s.root, className, {
+				[s.black]: isBlack,
+			})}
+			onClick={handleClick}
+		>
+			{children}
+		</button>
+	);
+};
 
 Button.defaultProps = {
-	blackbtn: false,
+	isBlack: false,
 };
 
 Button.propTypes = {
-    blackbtn: PropTypes.bool,
-    className: PropTypes.string,
-    children: PropTypes.node
-}
+	isBlack: PropTypes.bool,
+	className: PropTypes.string,
+	children: PropTypes.node,
+};
 
 export default Button;
