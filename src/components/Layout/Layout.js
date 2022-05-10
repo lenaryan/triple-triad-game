a9deb1028188c8f71dcc6b64dc51f8851349cb91
Elena Nichutina <elena.nichutina@x5.ru>
2022-05-10 12:43:04 +0300

    remove unused prop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import Header from '../Header';
 import Footer from '../Footer';
 import { Outlet } from 'react-router-dom';
 
-const Layout = ({ children }) => {
+const Layout = () => {
 	return (
 		<>
 			<Header />
